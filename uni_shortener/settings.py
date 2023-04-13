36ea9441b25d91e2ef7c72b18d12cc7d07166b55
zNitche <53448802+zNitche@users.noreply.github.com>
2023-04-13 23:31:09 +0200

    base templates + static files setup

--- uni_shortener/settings.py
+++ uni_shortener/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'uni_shortener.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "base_templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,6 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
