88e2b534ee799bbb7bc0818c2e1b6069c78a9d56
zNitche <53448802+zNitche@users.noreply.github.com>
2023-04-23 09:01:16 +0200

    added characters validation for SecondFAForm

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -7,4 +7,14 @@ class LoginForm(forms.Form):
 
 
 class SecondFAForm(forms.Form):
-    auth_code = forms.CharField(label="code", max_length=6)
+    auth_code = forms.CharField(label="code", max_length=6, min_length=6)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        auth_code = cleaned_data.get("auth_code")
+
+        try:
+            int(auth_code)
+
+        except ValueError as e:
+            raise forms.ValidationError("authentication code can contain only digits")
